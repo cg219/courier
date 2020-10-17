9fa33218e04f22aa959949bbcac9af81fbd0031b
Clemente <dev@imkreative.com>
2020-10-17 03:31:16 -0400

    Post

--- index.js
+++ index.js
@@ -45,7 +45,62 @@ const get = async (endpoint, opts) => {
     })
 }
 
+const post = async (endpoint, opts) => {
+    const url = new URL(endpoint);
+
+    if (!['https:', 'http:'].includes(url.protocol.toLowerCase())) throw Error('Courier is only for http or https calls');
+
+    let bodyData;
+    const { body, params, responseType, ...options } = opts || {};
+    const protocol = url.protocol === 'http:' ? http : https;
+    const defaults = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    if (options) options.method = 'POST';
+    if (params) {
+        url.search = new URLSearchParams(params).toString();
+    }
+    if (body) {
+        bodyData = JSON.stringify(body);
+        defaults.headers['Content-Length'] = Buffer.byteLength(bodyData);
+    }
+
+    const finalOptions = { ...defaults, ...options }
+
+    return new Promise((resolve, reject) => {
+        const request = protocol.request(url, finalOptions, response => {
+            let buffer;
+
+            response.on('data', data => buffer = buffer ? Buffer.concat([buffer, data]) : Buffer.from(data));
+            response.on('error', reject);
+            response.on('end', () => transformData(buffer.toString()));
+        });
+
+        const transformData = data => {
+            const transform = responstTypes[responseType];
+            if (transform) return resolve(transform(data));
+
+            return resolve(responstTypes['json'](data));
+        }
+
+        request.on('error', reject);
+        if (body) request.write(bodyData);
+        request.end();
+    })
+}
+
 (async function () {
-    const response = await get('https://jsonplaceholder.typicode.com/users');
+    const response = await post('https://jsonplaceholder.typicode.com/users', {
+        body: {
+            email: 'dev@imkreative.com',
+            pass: 'password',
+            message: 'Testing to see if this is working'
+        }
+    });
+    console.log(response)
 })()
 
